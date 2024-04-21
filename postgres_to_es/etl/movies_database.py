89e12c76b515ea4d1696e2a4fe15546c9e95bded
kseravin <kseravin@freeconferencecall.com>
2024-04-21 08:14:55 +0300

    improve logging, naming

--- postgres_to_es/etl/movies_database.py
+++ postgres_to_es/etl/movies_database.py
@@ -1,11 +1,11 @@
 import os
-from typing import Any, List, Iterator, Optional
+from typing import Any, Dict, List, Iterator, Optional
 from uuid import UUID
 from datetime import datetime
 
 import psycopg
 from psycopg.rows import dict_row
-from psycopg.errors import ConnectionException
+from psycopg.errors import OperationalError
 from dotenv import load_dotenv
 
 from etl.search_engine import SearchEngineFilmwork
@@ -32,17 +32,18 @@ def get_updated_filmworks() -> Iterator[List[SearchEngineFilmwork]]:
 
 def _get_filmorks_with_updated_personas() -> Iterator[List[SearchEngineFilmwork]]:
     logger.info("Getting filmworks with updated personas")
-    yield from _get_filmorks_with_updated_entities("person")
+    yield from _get_filmorks_with_updated_related_entities("person")
 
 
 def _get_filmorks_with_updated_genres() -> Iterator[List[SearchEngineFilmwork]]:
     logger.info("Getting filmworks with updated genres")
-    yield from _get_filmorks_with_updated_entities("genre")
+    yield from _get_filmorks_with_updated_related_entities("genre")
 
 
 def _get_updated_filmworks() -> Iterator[List[SearchEngineFilmwork]]:
     logger.info("Getting updated filmworks")
-    state_key = "filmwork_last_seen_modified"
+    state_key = _get_state_key("filmwork")
+    seen_filmworks_count = 0
     should_generate = True
     while should_generate:
         last_seen_modified = state.get(state_key)
@@ -50,20 +51,22 @@ def _get_updated_filmworks() -> Iterator[List[SearchEngineFilmwork]]:
         filmworks = _db_execute(cmd)
         if filmworks:
             state.set(state_key, filmworks[-1]["modified"])
-            yield [SearchEngineFilmwork.parse_obj(fw) for fw in filmworks]
+            seen_filmworks_count += len(filmworks)
+            yield _normalize_filmworks(filmworks)
         else:
             should_generate = False
-            logger.info(f"Seen updates for filmwork: {last_seen_modified}")
+            logger.info(f"Seen {seen_filmworks_count} updates for filmwork: {last_seen_modified}")
 
 
-def _get_filmorks_with_updated_entities(entity: str) -> Iterator[List[SearchEngineFilmwork]]:
-    for entity_ids_ids in _get_updated_entities(entity):
-        for filmwork_ids in _get_filmworks_ids_with_entities(entity, entity_ids_ids):
+def _get_filmorks_with_updated_related_entities(entity: str) -> Iterator[List[SearchEngineFilmwork]]:
+    for entity_ids_ids in _get_updated_related_entities_ids(entity):
+        for filmwork_ids in _get_filmworks_ids_with_related_entities(entity, entity_ids_ids):
             yield _get_filmworks_by_ids(filmwork_ids)
 
 
-def _get_updated_entities(entity: str) -> Iterator[List[UUID]]:
-    state_key = f"{entity}_last_seen_modified"
+def _get_updated_related_entities_ids(entity: str) -> Iterator[List[UUID]]:
+    state_key = _get_state_key(entity)
+    seen_entities_count = 0
     should_generate = True
     while should_generate:
         last_seen_modified = state.get(state_key)
@@ -71,22 +74,23 @@ def _get_updated_entities(entity: str) -> Iterator[List[UUID]]:
         entities = _db_execute(cmd)
         if entities:
             state.set(state_key, entities[-1]["modified"])
-            yield {e["id"] for e in entities}
+            seen_entities_count += len(entities)
+            yield [e["id"] for e in entities]
         else:
             should_generate = False
-            logger.info(f"Seen updates for {entity}: {last_seen_modified}")
+            logger.info(f"Seen {seen_entities_count} updates for {entity}: {last_seen_modified}")
 
 
-def _get_filmworks_ids_with_entities(entity: str, entity_ids: List[UUID]) -> Iterator[List[UUID]]:
+def _get_filmworks_ids_with_related_entities(entity: str, entity_ids: List[UUID]) -> Iterator[List[UUID]]:
     should_generate = True
-    state_key = f"{entity}_filmwork_last_seen_modified"
+    state_key = _get_state_key(f"{entity}_filmwork")
     while should_generate:
         last_seen_modified = state.get(state_key)
         cmd = _build_sql_requesting_filmworks_ids_with_entity(entity, entity_ids, last_seen_modified)
         filmworks = _db_execute(cmd)
         if filmworks:
             state.set(state_key, filmworks[-1]["modified"])
-            yield {fw["id"] for fw in filmworks}
+            yield [fw["id"] for fw in filmworks]
         else:
             state.reset(state_key)
             should_generate = False
@@ -94,18 +98,22 @@ def _get_filmworks_ids_with_entities(entity: str, entity_ids: List[UUID]) -> Ite
 
 def _get_filmworks_by_ids(filmwork_ids: List[UUID]) -> List[SearchEngineFilmwork]:
     cmd = _build_sql_requesting_filmworks(filmwork_ids)
-    return [SearchEngineFilmwork.parse_obj(fw) for fw in _db_execute(cmd)]
+    return _normalize_filmworks(_db_execute(cmd))
 
 
-@backoff(exceptions=(ConnectionException,))
+@backoff(exceptions=(OperationalError,))
 def _db_execute(cmd: str) -> Any:
     with conn.cursor() as curs:
         return curs.execute(cmd).fetchall()
 
 
+def _normalize_filmworks(filmworks: List[Dict[str, Any]]) -> List[SearchEngineFilmwork]:
+    return [SearchEngineFilmwork.parse_obj(fw) for fw in filmworks]
+
+
 def _build_sql_requesting_entity(entity: str, last_seen_modified: datetime) -> str:
     return f"""
-        SELECT id, modified
+        SELECT DISTINCT id, modified
         FROM content.{entity}
         WHERE modified > '{last_seen_modified.isoformat()}'
         ORDER BY modified
@@ -117,7 +125,7 @@ def _build_sql_requesting_filmworks_ids_with_entity(
         entity: str, entity_ids: List[UUID], last_seen_modified: datetime) -> str:
     normalized_entity_ids = ' ,'.join([f"'{str(id)}'" for id in entity_ids])
     return f"""
-        SELECT fw.id, fw.modified
+        SELECT DISTINCT fw.id, fw.modified
         FROM content.film_work fw
         LEFT JOIN content.{entity}_film_work efw ON efw.film_work_id = fw.id
         WHERE efw.{entity}_id IN ({normalized_entity_ids}) AND
@@ -155,3 +163,7 @@ def _build_sql_requesting_filmworks(ids: Optional[List[UUID]] = None,
         ORDER BY fw.modified
         {limit}
     """
+
+
+def _get_state_key(prefix: str) -> str:
+    return f"{prefix}_last_seen_modified"
