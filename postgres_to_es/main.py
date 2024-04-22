995ac013147d985ad4cd6f7d5973c801644b3890
kseravin <kseravin@freeconferencecall.com>
2024-04-22 10:10:46 +0300

    rename main function

--- postgres_to_es/main.py
+++ postgres_to_es/main.py
@@ -5,7 +5,7 @@ from etl import movies_database, search_engine
 _MOVIES_DATABASE_UPDATES_CHECK_PERIOD_SECONDS = 10
 
 
-def main() -> None:
+def transfer_updated_filmworks() -> None:
     for filmworks_chunk in movies_database.get_updated_filmworks():
         search_engine.load(filmworks_chunk)
 
@@ -13,5 +13,5 @@ def main() -> None:
 if __name__ == "__main__":
     search_engine.create_index()
     while True:
-        main()
+        transfer_updated_filmworks()
         time.sleep(_MOVIES_DATABASE_UPDATES_CHECK_PERIOD_SECONDS)
