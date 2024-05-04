80ec4f31711180512ed5fcc2f216c52086e95cac
kseravin <kseravin@freeconferencecall.com>
2024-05-04 15:50:58 +0300

    refactor movies database module

--- postgres_to_es/etl/movies_database.py
+++ postgres_to_es/etl/movies_database.py
@@ -1,6 +1,7 @@
-from typing import Any, Dict, List, Iterator, Optional
+from typing import Any, Dict, List, Iterator, Optional, Callable
 from uuid import UUID
 from datetime import datetime
+from enum import Enum
 
 from psycopg.rows import dict_row
 from psycopg.errors import OperationalError
@@ -22,83 +23,61 @@ conninfo = ("postgresql://"
 conn_pool = ConnectionPool(conninfo, min_size=1, max_size=1)
 
 
+class Entity(str, Enum):
+    FILMWORK = "filmwork"
+    PERSON = "person"
+    GENRE = "genre"
+
+
 def get_updated_genres() -> Iterator[List[SearchEngineGenre]]:
-    logger.info("Getting updated genres")
-    state_key = _get_state_key("genre")
-    seen_genres_count = 0
-    should_generate = True
-    while should_generate:
-        last_seen_modified = state.get(state_key)
-        cmd = _build_sql_requesting_genres(last_seen_modified=last_seen_modified)
-        genres = _db_execute(cmd)
-        if genres:
-            state.save(state_key, genres[-1]["modified"])
-            seen_genres_count += len(genres)
-            yield _normalize(genres, SearchEngineGenre)
-        else:
-            should_generate = False
-            logger.info("Seen %s updates for genres: %s", seen_genres_count, last_seen_modified)
+    yield from _get_updated_entity(Entity.GENRE)
 
 
 def get_updated_personas() -> Iterator[List[SearchEnginePerson]]:
-    logger.info("Getting updated personas")
-    state_key = _get_state_key("person")
-    seen_personas_count = 0
-    should_generate = True
-    while should_generate:
-        last_seen_modified = state.get(state_key)
-        cmd = _build_sql_requesting_personas(last_seen_modified=last_seen_modified)
-        personas = _db_execute(cmd)
-        if personas:
-            state.save(state_key, personas[-1]["modified"])
-            seen_personas_count += len(personas)
-            yield _normalize(personas, SearchEnginePerson)
-        else:
-            should_generate = False
-            logger.info("Seen %s updates for personas: %s", seen_personas_count, last_seen_modified)
+    yield from _get_updated_entity(Entity.PERSON)
 
 
 def get_updated_filmworks() -> Iterator[List[SearchEngineFilmwork]]:
     yield from _get_filmorks_with_updated_personas()
     yield from _get_filmorks_with_updated_genres()
-    yield from _get_updated_filmworks()
+    yield from _get_updated_entity(Entity.FILMWORK)
 
 
 def _get_filmorks_with_updated_personas() -> Iterator[List[SearchEngineFilmwork]]:
     logger.info("Getting filmworks with updated personas")
-    yield from _get_filmorks_with_updated_related_entities("person")
+    yield from _get_filmorks_with_updated_related_entities(Entity.PERSON)
 
 
 def _get_filmorks_with_updated_genres() -> Iterator[List[SearchEngineFilmwork]]:
     logger.info("Getting filmworks with updated genres")
-    yield from _get_filmorks_with_updated_related_entities("genre")
+    yield from _get_filmorks_with_updated_related_entities(Entity.GENRE)
 
 
-def _get_updated_filmworks() -> Iterator[List[SearchEngineFilmwork]]:
-    logger.info("Getting updated filmworks")
-    state_key = _get_state_key("filmwork")
-    seen_filmworks_count = 0
+def _get_updated_entity(entity: Entity) -> Iterator[BaseModel]:
+    logger.info("Getting updated %s", entity)
+    state_key = _get_state_key(entity)
+    sql_builder = _get_sql_builder(entity)
+    seen_entities_count = 0
     should_generate = True
     while should_generate:
         last_seen_modified = state.get(state_key)
-        cmd = _build_sql_requesting_filmworks(last_seen_modified=last_seen_modified)
-        filmworks = _db_execute(cmd)
-        if filmworks:
-            state.save(state_key, filmworks[-1]["modified"])
-            seen_filmworks_count += len(filmworks)
-            yield _normalize(filmworks, SearchEngineFilmwork)
+        entities = _db_execute(sql_builder(last_seen_modified=last_seen_modified))
+        if entities:
+            state.save(state_key, entities[-1]["modified"])
+            seen_entities_count += len(entities)
+            yield _normalize(entities, _get_model(entity))
         else:
             should_generate = False
-            logger.info("Seen %s updates for filmwork: %s", seen_filmworks_count, last_seen_modified)
+            logger.info("Seen %s updates for %s: %s", seen_entities_count, entity, last_seen_modified)
 
 
-def _get_filmorks_with_updated_related_entities(entity: str) -> Iterator[List[SearchEngineFilmwork]]:
+def _get_filmorks_with_updated_related_entities(entity: Entity) -> Iterator[List[SearchEngineFilmwork]]:
     for entity_ids_ids in _get_updated_related_entities_ids(entity):
         for filmwork_ids in _get_filmworks_ids_with_related_entities(entity, entity_ids_ids):
             yield _get_filmworks_by_ids(filmwork_ids)
 
 
-def _get_updated_related_entities_ids(entity: str) -> Iterator[List[UUID]]:
+def _get_updated_related_entities_ids(entity: Entity) -> Iterator[List[UUID]]:
     state_key = _get_state_key(f"{entity}_related")
     seen_entities_count = 0
     should_generate = True
@@ -115,7 +94,7 @@ def _get_updated_related_entities_ids(entity: str) -> Iterator[List[UUID]]:
             logger.info("Seen %s updates for %s: %s", seen_entities_count, entity, last_seen_modified)
 
 
-def _get_filmworks_ids_with_related_entities(entity: str, entity_ids: List[UUID]) -> Iterator[List[UUID]]:
+def _get_filmworks_ids_with_related_entities(entity: Entity, entity_ids: List[UUID]) -> Iterator[List[UUID]]:
     should_generate = True
     state_key = _get_state_key(f"{entity}_filmwork")
     while should_generate:
@@ -142,11 +121,21 @@ def _db_execute(cmd: str) -> List[Dict[str, Any]]:
         return conn.execute(cmd).fetchall()
 
 
+def _get_model(entity: Entity) -> BaseModel:
+    if entity == Entity.FILMWORK:
+        model = SearchEngineFilmwork
+    elif entity == Entity.GENRE:
+        model = SearchEngineGenre
+    else:
+        model = SearchEnginePerson
+    return model
+
+
 def _normalize(data: List[Dict[str, Any]], model: BaseModel) -> List[BaseModel]:
     return [model.parse_obj(i) for i in data]
 
 
-def _build_sql_requesting_entity(entity: str, last_seen_modified: datetime) -> str:
+def _build_sql_requesting_entity(entity: Entity, last_seen_modified: datetime) -> str:
     return f"""
         SELECT DISTINCT id, modified
         FROM content.{entity}
@@ -157,7 +146,7 @@ def _build_sql_requesting_entity(entity: str, last_seen_modified: datetime) -> s
 
 
 def _build_sql_requesting_filmworks_ids_with_entity(
-        entity: str, entity_ids: List[UUID], last_seen_modified: datetime) -> str:
+        entity: Entity, entity_ids: List[UUID], last_seen_modified: datetime) -> str:
     normalized_entity_ids = ' ,'.join([f"'{str(id)}'" for id in entity_ids])
     return f"""
         SELECT DISTINCT fw.id, fw.modified
@@ -226,6 +215,16 @@ def _build_sql_requesting_personas(last_seen_modified: datetime) -> str:
         ORDER BY p.modified
         LIMIT {_CHUNK_SIZE}
     """
+
+
+def _get_sql_builder(entity: Entity) -> Callable[[datetime], str]:
+    if entity == Entity.FILMWORK:
+        builder = _build_sql_requesting_filmworks
+    elif entity == Entity.GENRE:
+        builder = _build_sql_requesting_genres
+    else:
+        builder = _build_sql_requesting_personas
+    return builder
 
 
 def _get_state_key(prefix: str) -> str:
